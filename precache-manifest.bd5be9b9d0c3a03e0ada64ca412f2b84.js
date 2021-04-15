27f30abbf72bbd84d3b8ddc69878e5d72ecb9c79
Theo Bekhedda <theobekhedda@MacBook-Air-de-Theo.local>
2021-04-15 16:18:34 +0200

    Updates

---
+++ precache-manifest.bd5be9b9d0c3a03e0ada64ca412f2b84.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4315d1a83f8309ea9848bb4c5bcd9f97",
+    "url": "/blindtest-spotify/index.html"
+  },
+  {
+    "revision": "75c63173a83decbad52c",
+    "url": "/blindtest-spotify/static/css/main.edb96105.chunk.css"
+  },
+  {
+    "revision": "3af7a84849e26c137123",
+    "url": "/blindtest-spotify/static/js/2.7877c585.chunk.js"
+  },
+  {
+    "revision": "acba050d3010a45dfd3fbba97dedcd92",
+    "url": "/blindtest-spotify/static/js/2.7877c585.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "75c63173a83decbad52c",
+    "url": "/blindtest-spotify/static/js/main.ec921ab3.chunk.js"
+  },
+  {
+    "revision": "378028445f9cb0e378bd",
+    "url": "/blindtest-spotify/static/js/runtime-main.0c270892.js"
+  },
+  {
+    "revision": "9d4cbf40dcdac09bda6fa0aff397246b",
+    "url": "/blindtest-spotify/static/media/loading.9d4cbf40.svg"
+  },
+  {
+    "revision": "79f6b6a0c4f3cca321dc458fe7e6544a",
+    "url": "/blindtest-spotify/static/media/logo.79f6b6a0.svg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/blindtest-spotify/precache-manifest.af1d10f296fb8bac425bec0e56195ae1.js"
+  "/blindtest-spotify/precache-manifest.bd5be9b9d0c3a03e0ada64ca412f2b84.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -35,5 +35,5 @@ workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
 workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/blindtest-spotify/index.html"), {
   
-  blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
+  blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
